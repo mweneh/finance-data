290076c430be1aa16827272fe4c7b9a73f2d5ea8
David <davidongaroh@gmail.com>
2024-06-05 15:18:38 +0300

    created newsletter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-
+import Newsletter from './components/Newsletter';
+import Analytics from './components/Analytics';
 function App() {
   return (
     <div>
     <Navbar />
     < Hero />
+    <Analytics />
+    <Newsletter />
     </div>
   );
 }
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Newsletter() {
+  return (
+    <div className='w-full py-16 text-white'>
+        
+    </div>
+  )
+}
+
+export default Newsletter
